e0522ba6e16d9079fe51b1987b5ac3b66baac6de
Jefrijefri92-lgtm <Jefri.jefri92@yahoo.com>
2026-05-12 09:51:56 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,10 +1,15 @@
 from http.server import BaseHTTPRequestHandler
+import os
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
-        html = '<h1>UDAH BISA JEFRI!</h1><p>Typo ginocorn kelar. 5 jam 10 menit tamat.</p>'
+        
+        file_path = os.path.join(os.path.dirname(__file__), '..', 'templates', 'index.html')
+        with open(file_path, 'r', encoding='utf-8') as f:
+            html = f.read()
+            
         self.wfile.write(html.encode())
         return
