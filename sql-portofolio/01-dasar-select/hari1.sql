64b9e015d915d2afa65acb10b171be5a71df44a5
Jefrijefri92-lgtm <Jefri.jefri92@yahoo.com>
2026-05-26 11:19:46 +0700

    Update hari1.sql

--- sql-portofolio/01-dasar-select/hari1.sql
+++ sql-portofolio/01-dasar-select/hari1.sql
@@ -1,15 +1,36 @@
-filter kota saja
+
+1. Tampilkan semua data
+```sql
+  CREATE TABLE penjualan (
+    id INT,
+    tanggal DATE,
+    kota VARCHAR(20),
+    produk VARCHAR(20),
+    jumlah INT,
+    harga INT
+);
+
+INSERT INTO penjualan VALUES
+(1, '2024-01-05', 'Jakarta', 'Kopi Hitam', 50, 15000),
+(2, '2024-01-06', 'Bandung', 'Latte', 30, 25000),
+(3, '2024-01-07', 'Jakarta', 'Cappuccino', 40, 22000),
+(4, '2024-01-08', 'Surabaya', 'Kopi Hitam', 60, 15000),
+(5, '2024-01-09', 'Bandung', 'Kopi Hitam', 25, 15000),
+(6, '2024-01-10', 'Jakarta', 'Latte', 35, 25000);
+    
+2. filter kota saja  
+SELECT * FROM penjualan;
 SELECT * FROM penjualan WHERE kota = 'Jakarta';
 
-menampilkan kopi hitam dengan jumlah lebih dari 30
+3. menampilkan kopi hitam dengan jumlah lebih dari 30
 SELECT * FROM penjualan 
 WHERE produk = 'Kopi Hitam' AND jumlah > 30;
 
-Urutkan dari harga termahal
+4. Urutkan dari harga termahal
 SELECT * FROM penjualan 
 ORDER BY harga DESC;
 
-Ambil 2 transaksi terakhir
+5. Ambil 2 transaksi terakhir
 SELECT * FROM penjualan 
 ORDER BY tanggal DESC 
 LIMIT 2;
